1e4553b2aa8e3f96ffc49bc84a9996091239e716
nith822 <1harveygao@gmail.com>
2016-08-25 23:07:15 -0500

    Added TiledMap to Level and the Player-Centric-Camera to GameScreen

--- src/com/github/houkagoteatime/levels/Level.java
+++ src/com/github/houkagoteatime/levels/Level.java
@@ -3,17 +3,37 @@ package com.github.houkagoteatime.levels;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import com.badlogic.gdx.maps.MapProperties;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.github.houkagoteatime.entities.Player;
 import com.github.houkagoteatime.entities.enemies.Enemy;
 
 public abstract class Level {
 	
+	private TiledMap tiledMap;
+	private TiledMapRenderer tiledMapRenderer;
+	
+	public MapProperties mapProp;
+	public int mapWidth;
+	public int mapHeight;
+	public int tilePixelWidth;
+	public int tilePixelHeight;
+	public int mapPixelWidth;
+	public int mapPixelHeight;
+	    
+
 	private Player player;
 	private ArrayList<Enemy> enemies;
 	//private ArrayList<Projectiles> projectiles;
 	
 	public Level(String path) {
-		loadLevel(path);
+		setTiledMap(path);
+		setTiledMapRenderer(tiledMap);
+		setMapProperties(tiledMap);
+		calcMapProperties(mapProp);
 	}
 	
 	/**
@@ -44,22 +64,29 @@ public abstract class Level {
 	    }*/
 	}
 	
-	public void loadLevel(String path) {
+	public void calcMapProperties(MapProperties mapProp) {
 		
+		 mapWidth = mapProp.get("width", Integer.class);
+	     mapHeight = mapProp.get("height", Integer.class);
+	     tilePixelWidth = mapProp.get("tilewidth", Integer.class);
+	     tilePixelHeight = mapProp.get("tileheight", Integer.class);
+
+	     mapPixelWidth = mapWidth * tilePixelWidth;
+	     mapPixelHeight = mapHeight * tilePixelHeight;
 	}
 	
 	public void dispose() {
 		
 	}
 	
+	public Player getPlayer() {
+		return player;
+	}
+	
 	public void setPlayer(Player player) {
 		this.player = player;
 	}
 	
-	public Player getPlayer() {
-		return player;
-	}
-
 	public ArrayList<Enemy> getEnemies() {
 		return enemies;
 	}
@@ -67,4 +94,32 @@ public abstract class Level {
 	/*public ArrayList<Projectiles> getProjectiles() {
 		return projectiles;
 	}*/
+	
+
+	public TiledMap getTiledMap() {
+		return tiledMap;
+	}
+	
+	public void setTiledMap(String path) {
+		tiledMap  = new TmxMapLoader().load(path);
+	}
+	
+	public TiledMapRenderer getTiledMapRenderer() {
+		return tiledMapRenderer;
+	}
+	
+
+	public MapProperties getMapProperties() {
+		return mapProp;
+		
+	}
+	
+
+	public void setMapProperties(TiledMap tiledMap) {
+		mapProp = tiledMap.getProperties();
+	}
+	
+	public void setTiledMapRenderer(TiledMap tiledMap) {
+		tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+	}
 }
--- src/com/github/houkagoteatime/screens/GameScreen.java
+++ src/com/github/houkagoteatime/screens/GameScreen.java
@@ -1,12 +1,17 @@
 package com.github.houkagoteatime.screens;
 
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.github.houkagoteatime.entities.Player;
 import com.github.houkagoteatime.entities.enemies.Enemy;
 import com.github.houkagoteatime.levels.Level;
 
 public class GameScreen implements Screen{
 
+
+    private OrthographicCamera cam;
 	private SpriteBatch sb;
 	private Level level;
 	
@@ -19,8 +24,14 @@ public class GameScreen implements Screen{
 	 */
 	@Override
 	public void render(float dt) {
+		
 		level.update(dt);
+		
 		sb.begin();
+		
+		level.getTiledMapRenderer().setView(cam);
+        level.getTiledMapRenderer().render();
+        
 		for(Enemy enemy : level.getEnemies()){
 			sb.draw(enemy.getSprite(), enemy.getPosition().x, enemy.getPosition().y);
 		}
@@ -30,6 +41,20 @@ public class GameScreen implements Screen{
 		/*for(Weapon.Projectile projectile : projectiles){
 			projectile.draw(sb);
 		}*/
+	}
+	
+	/**
+	 * @param player center the camera around player
+	 */
+	public void updateCam(Player player) {
+		
+		//cam zoom shouldn't be changed much, should make a seperate function to change it
+		float effectiveViewportWidth = cam.viewportWidth * cam.zoom;
+        float effectiveViewportHeight = cam.viewportHeight * cam.zoom;
+        
+        //Make sure the camera stays within the range of the map, no black space shown on screen
+		cam.position.x = MathUtils.clamp(player.getSprite().getHeight()/2, effectiveViewportWidth / 2f, level.mapPixelWidth - effectiveViewportWidth / 2f);
+	    cam.position.y = MathUtils.clamp(player.getSprite().getWidth()/2, effectiveViewportHeight / 2f, level.mapPixelHeight - effectiveViewportWidth / 2f);
 	}
 	
 	@Override
